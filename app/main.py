03a6adc746d439b2fd59285e571fb808b54f0c0e
arun-j0 <159032521+arun-j0@users.noreply.github.com>
2024-08-30 21:49:07 +0530

    aded cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,24 @@
 from fastapi import FastAPI
 from app.routers import calculator
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 app.include_router(calculator.router)
 
