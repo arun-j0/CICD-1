3af66081988c0ea3ddc01bb3f9f52cec88153bdb
arun-j0 <159032521+arun-j0@users.noreply.github.com>
2024-08-30 21:43:17 +0530

    removed cors

--- app/main.py
+++ app/main.py
@@ -5,14 +5,6 @@ app = FastAPI()
 
 app.include_router(calculator.router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[*],  # Replace with your frontend's URL
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 
 @app.get("/")
